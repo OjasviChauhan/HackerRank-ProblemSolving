7d12f12891d4958799be20518c0db9d6d3b2b9a5
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2019-09-06 16:55:19 +0530

    Update aVeryBigSum.c

--- aVeryBigSum.c
+++ aVeryBigSum.c
@@ -12,15 +12,15 @@ long aVeryBigSum(int ar_count, long* ar)
 int main()
 {
   int ar_count;
- // printf("Enter the size of array\n");
+  printf("Enter the size of array\n");
   scanf("%d",&ar_count);
   
   long ar[ar_count];
- // printf("Enter the elements of array\n");
+  printf("Enter the elements of array\n");
   for(int i=0;i<ar_count;i++)
   scanf("%ld",&ar[i]);
 
- // printf("Sum of the elements of the array would be ");
+  printf("Sum of the elements of the array would be ");
   long sum=aVeryBigSum(ar_count, ar);
   printf("%ld",sum);
 }
