b0eed5541542ba10931ea0934dc982afadd4c360
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-01-24 19:19:17 +0530

    Update Cats & a Mouse.java

--- Cats & a Mouse.java
+++ Cats & a Mouse.java
@@ -19,24 +19,16 @@ public class Solution {
              return M;
     }
     private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
         int q = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
         for (int qItr = 0; qItr < q; qItr++) {
             String[] xyz = scanner.nextLine().split(" ");
-
             int x = Integer.parseInt(xyz[0]);
-
             int y = Integer.parseInt(xyz[1]);
-
             int z = Integer.parseInt(xyz[2]);
-
             String result = catAndMouse(x, y, z);
-
             bufferedWriter.write(result);
             bufferedWriter.newLine();
         }
