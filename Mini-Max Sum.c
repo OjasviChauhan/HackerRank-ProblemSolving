10027eeda8391ea2ba4d65b0c5025f2001e75911
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-01-31 22:56:09 +0530

    Update Mini-Max Sum.c

--- Mini-Max Sum.c
+++ Mini-Max Sum.c
@@ -30,15 +30,11 @@ long temp;
     for(int k=1;k<arr_count;k++)
         M=M+arr[k];
     printf("%ld %ld",m,M);
-
 }
-
 int main()
 {
     char** arr_temp = split_string(readline());
-
     long* arr = malloc(5 * sizeof(long));
-
     for (int i = 0; i < 5; i++) {
         char* arr_item_endptr;
         char* arr_item_str = *(arr_temp + i);
@@ -48,62 +44,42 @@ int main()
 
         *(arr + i) = arr_item;
     }
-
     int arr_count = 5;
-
     miniMaxSum(arr_count, arr);
-
     return 0;
 }
-
 char* readline() {
     size_t alloc_length = 1024;
     size_t data_length = 0;
     char* data = malloc(alloc_length);
-
     while (true) {
         char* cursor = data + data_length;
         char* line = fgets(cursor, alloc_length - data_length, stdin);
-
         if (!line) { break; }
-
         data_length += strlen(cursor);
-
         if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
-
         size_t new_length = alloc_length << 1;
         data = realloc(data, new_length);
-
         if (!data) { break; }
-
         alloc_length = new_length;
     }
-
     if (data[data_length - 1] == '\n') {
         data[data_length - 1] = '\0';
     }
-
     data = realloc(data, data_length);
-
     return data;
 }
-
 char** split_string(char* str) {
     char** splits = NULL;
     char* token = strtok(str, " ");
-
     int spaces = 0;
-
     while (token) {
         splits = realloc(splits, sizeof(char*) * ++spaces);
         if (!splits) {
             return splits;
         }
-
         splits[spaces - 1] = token;
-
         token = strtok(NULL, " ");
     }
-
     return splits;
 }
